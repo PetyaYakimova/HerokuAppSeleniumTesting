583410f88ec1954159425b4b17b4a5f9fe59b2d5
Petya Yakimova <petya_yakimova@abv.bg>
2024-10-22 09:41:08 +0300

    Add constructor in BasePage

--- HerokuAppSeleniumTesting/Pages/BasePage.cs
+++ HerokuAppSeleniumTesting/Pages/BasePage.cs
@@ -1,6 +1,23 @@
-﻿namespace HerokuAppSeleniumTesting.Pages
+﻿using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium;
+using HerokuAppSeleniumTesting.Setup;
+
+namespace HerokuAppSeleniumTesting.Pages
 {
 	public class BasePage
 	{
+		protected readonly AppSettings settings;
+		protected readonly IWebDriver driver;
+		protected readonly WebDriverWait wait;
+
+		protected BasePage(
+			AppSettings settings,
+			IWebDriver driver,
+			WebDriverWait wait)
+		{
+			this.settings = settings;
+			this.driver = driver;
+			this.wait = wait;
+		}
 	}
 }
