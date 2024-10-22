5530105150c88f8e34ecd5807aa7b4943220f6ae
Petya Yakimova <petya_yakimova@abv.bg>
2024-10-22 09:40:51 +0300

    Add AppSettings

---
+++ HerokuAppSeleniumTesting/Setup/AppSettings.cs
@@ -0,0 +1,12 @@
+﻿namespace HerokuAppSeleniumTesting.Setup
+{
+	public class AppSettings
+	{
+		public DomainSettings DomainSettings { get; set; } = null!;
+	}
+
+	public class DomainSettings
+	{
+		public string Domain { get; set; } = null!;
+	}
+}
