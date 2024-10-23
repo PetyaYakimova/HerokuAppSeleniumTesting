6643068419a6090b7855266665ff8ed4a548f76a
Petya Yakimova <petya_yakimova@abv.bg>
2024-10-23 09:13:26 +0300

    Add setup hook class

---
+++ HerokuAppSeleniumTesting/Setup/SetUpHook.cs
@@ -0,0 +1,58 @@
+﻿using BoDi;
+using Microsoft.Extensions.Configuration;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium;
+using TechTalk.SpecFlow;
+
+namespace HerokuAppSeleniumTesting.Setup
+{
+	[Binding]
+	public class SetUpHook
+	{
+		private readonly IConfigurationRoot configuration;
+		private WebDriverWait wait;
+		private readonly AppSettings settings;
+
+		public SetUpHook(IObjectContainer objectContainer)
+		{
+			configuration = BuildConfiguration();
+			settings = configuration.Get<AppSettings>();
+			objectContainer.RegisterInstanceAs(this.settings);
+		}
+
+		[BeforeScenario]
+		public void Setup(IObjectContainer objectContainer, ScenarioContext scenarioContext)
+		{
+			ChromeOptions options = new ChromeOptions();
+
+			options.AddArgument("--headless");
+			options.AddArgument("--enable-automation");
+			options.AddArgument("--start-maximized");
+			options.PageLoadStrategy = PageLoadStrategy.Normal;
+			IWebDriver driver = new ChromeDriver(".", options);
+			driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(10);
+			this.wait = new WebDriverWait(driver, new TimeSpan(0, 0, 10));
+			objectContainer.RegisterInstanceAs(driver);
+			objectContainer.RegisterInstanceAs(wait);
+		}
+
+		[AfterScenario]
+		public void TearDown(IObjectContainer objectContainer)
+		{
+			IWebDriver driver = objectContainer.Resolve<IWebDriver>();
+			driver.Quit();
+		}
+
+		private static IConfigurationRoot BuildConfiguration()
+		{
+			ConfigurationBuilder builder = new();
+
+#if DEBUG
+			builder.AddJsonFile(Path.Combine(Directory.GetCurrentDirectory(), "appsettings.Debug.json"), optional: false, reloadOnChange: false);
+#endif
+
+			return builder.Build();
+		}
+	}
+}
