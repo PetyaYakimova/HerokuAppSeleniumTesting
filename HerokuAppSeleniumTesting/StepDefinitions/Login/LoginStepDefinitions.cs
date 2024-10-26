50fcf48181352abd44cb7449aa1868c9e2adeb94
Petya Yakimova <petya_yakimova@abv.bg>
2024-10-26 11:45:27 +0300

    Define login step

--- HerokuAppSeleniumTesting/StepDefinitions/Login/LoginStepDefinitions.cs
+++ HerokuAppSeleniumTesting/StepDefinitions/Login/LoginStepDefinitions.cs
@@ -1,9 +1,22 @@
-﻿using TechTalk.SpecFlow;
+﻿using HerokuAppSeleniumTesting.Pages.Login;
+using TechTalk.SpecFlow;
 
 namespace HerokuAppSeleniumTesting.StepDefinitions.Login
 {
 	[Binding]
 	public class LoginStepDefinitions
 	{
+		private readonly LoginPage loginPage;
+
+		public LoginStepDefinitions(LoginPage loginPage)
+		{
+			this.loginPage = loginPage;
+		}
+
+		[StepDefinition(@"I enter (.*) in the username field")]
+		public void IEnterValueInTheUsernameField(string value)
+		{
+			loginPage.EnterUsername(value);
+		}
 	}
 }
