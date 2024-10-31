e9fd48bf8d5d8eeb12a4490d0dae548832108738
Petya Yakimova <petya_yakimova@abv.bg>
2024-10-31 09:05:37 +0200

    Add step definition class for the add remove element feature

---
+++ HerokuAppSeleniumTesting/StepDefinitions/AddRemoveElements/AddRemoveElementsStepDefinitions.cs
@@ -0,0 +1,25 @@
+﻿using HerokuAppSeleniumTesting.Pages.AddRemoveElements;
+using TechTalk.SpecFlow;
+
+namespace HerokuAppSeleniumTesting.StepDefinitions.AddRemoveElements
+{
+	[Binding]
+	public class AddRemoveElementsStepDefinitions
+	{
+		private readonly AddRemoveElementsPage addRemoveElementsPage;
+
+		public AddRemoveElementsStepDefinitions(AddRemoveElementsPage addRemoveElementsPage)
+		{
+			this.addRemoveElementsPage = addRemoveElementsPage;
+		}
+
+		[StepDefinition(@"I click the Add element button (.*) times")]
+		public void ClickTheAddElementButtonMultipleTimes(int numberOfTimes)
+		{
+			for (int i = 0; i < numberOfTimes; i++)
+			{
+				addRemoveElementsPage.ClickAddElementButton();
+			}
+		}
+	}
+}
