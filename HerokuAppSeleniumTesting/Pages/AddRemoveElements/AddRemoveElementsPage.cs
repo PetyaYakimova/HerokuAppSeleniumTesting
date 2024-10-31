a765a6b8c52b2131afa579c4527f701e3ac18796
Petya Yakimova <petya_yakimova@abv.bg>
2024-10-31 09:05:21 +0200

    Add add remoce elements page and define first method and locator

---
+++ HerokuAppSeleniumTesting/Pages/AddRemoveElements/AddRemoveElementsPage.cs
@@ -0,0 +1,22 @@
+﻿using HerokuAppSeleniumTesting.Setup;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium;
+
+namespace HerokuAppSeleniumTesting.Pages.AddRemoveElements
+{
+	public class AddRemoveElementsPage : BasePage
+	{
+		public AddRemoveElementsPage(AppSettings settings, IWebDriver driver, WebDriverWait wait)
+			: base(settings, driver, wait)
+		{
+		}
+
+		public IWebElement AddElementButton => driver.FindElement(AddElementButtonBy);
+		public By AddElementButtonBy = By.XPath("//button[@onclick='addElement()']");
+
+		public void ClickAddElementButton()
+		{
+			AddElementButton.Click();
+		}
+	}
+}
