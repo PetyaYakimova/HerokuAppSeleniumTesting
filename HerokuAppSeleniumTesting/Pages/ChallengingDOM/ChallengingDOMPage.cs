03e27aa087343b3c30073bfd59796eea03036ba0
Petya Yakimova <petya_yakimova@abv.bg>
2024-11-04 08:51:31 +0200

    Add challenging DOM page class

---
+++ HerokuAppSeleniumTesting/Pages/ChallengingDOM/ChallengingDOMPage.cs
@@ -0,0 +1,14 @@
+﻿using HerokuAppSeleniumTesting.Setup;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium;
+
+namespace HerokuAppSeleniumTesting.Pages.ChallengingDOM
+{
+	public class ChallengingDOMPage : BasePage
+	{
+		public ChallengingDOMPage(AppSettings settings, IWebDriver driver, WebDriverWait wait)
+			: base(settings, driver, wait)
+		{
+		}
+	}
+}
