89c7069f5c2309cbe3c67c5c4715e17c2031fbec
Petya Yakimova <petya_yakimova@abv.bg>
2024-11-04 08:54:53 +0200

    Add locators for challenging DOM page

--- HerokuAppSeleniumTesting/Pages/ChallengingDOM/ChallengingDOMPage.cs
+++ HerokuAppSeleniumTesting/Pages/ChallengingDOM/ChallengingDOMPage.cs
@@ -10,5 +10,17 @@ namespace HerokuAppSeleniumTesting.Pages.ChallengingDOM
 			: base(settings, driver, wait)
 		{
 		}
+
+		public IWebElement FirstButton => driver.FindElement(FirstButtonBy);
+		public By FirstButtonBy = By.XPath("//a[@class='button']");
+
+		public IWebElement SecondButton => driver.FindElement(SecondButtonBy);
+		public By SecondButtonBy = By.XPath("//a[@class='button alert']");
+
+		public IWebElement ThirdButton => driver.FindElement(ThirdButtonBy);
+		public By ThirdButtonBy = By.XPath("//a[@class='button success']");
+
+		public IWebElement CanvasResultText => driver.FindElement(CanvasResultTextBy);
+		public By CanvasResultTextBy = By.Id("canvas");
 	}
 }
