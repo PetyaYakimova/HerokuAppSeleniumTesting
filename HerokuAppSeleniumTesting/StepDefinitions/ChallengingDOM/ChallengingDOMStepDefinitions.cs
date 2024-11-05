62d7476f8542210ede9ae7dede45e714cbb2427b
Petya Yakimova <petya_yakimova@abv.bg>
2024-11-05 09:47:49 +0200

    Add step definition for getting the canvas answer text and save it into the variable

--- HerokuAppSeleniumTesting/StepDefinitions/ChallengingDOM/ChallengingDOMStepDefinitions.cs
+++ HerokuAppSeleniumTesting/StepDefinitions/ChallengingDOM/ChallengingDOMStepDefinitions.cs
@@ -1,9 +1,23 @@
-﻿using TechTalk.SpecFlow;
+﻿using HerokuAppSeleniumTesting.Pages.ChallengingDOM;
+using HerokuAppSeleniumTesting.Setup;
+using TechTalk.SpecFlow;
 
 namespace HerokuAppSeleniumTesting.StepDefinitions.ChallengingDOM
 {
 	[Binding]
 	public class ChallengingDOMStepDefinitions
 	{
+		private readonly ChallengingDOMPage challengindDOMPage;
+
+		public ChallengingDOMStepDefinitions(ChallengingDOMPage challengindDOMPage)
+		{
+			this.challengindDOMPage = challengindDOMPage;
+		}
+
+		[StepDefinition(@"I get the canvas answer text")]
+		public void IGetTheCanvasAnswerText()
+		{
+			Variables.canvasAnswerText = challengindDOMPage.GetCanvasAnswerText();
+		}
 	}
 }
