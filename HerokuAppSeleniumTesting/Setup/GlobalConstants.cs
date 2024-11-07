7359447faac9de801fbed22a0c0bc97543298043
Petya Yakimova <petya_yakimova@abv.bg>
2024-11-07 08:56:51 +0200

    Add global constants

---
+++ HerokuAppSeleniumTesting/Setup/GlobalConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace HerokuAppSeleniumTesting.Setup
+{
+	public static class GlobalConstants
+	{
+		public static string ImageBeforeFilePath = @"..\..\..\Images\canvas_image_before.png";
+
+		public static string ImageAfterFilePath = @"..\..\..\Images\canvas_image_after.png";
+	}
+}
