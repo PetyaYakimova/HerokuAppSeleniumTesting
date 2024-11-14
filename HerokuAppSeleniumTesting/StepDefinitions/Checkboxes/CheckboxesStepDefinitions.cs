4cc7aa98ebf73d49cf3170ad32584fb3962c7bc3
Petya Yakimova <petya_yakimova@abv.bg>
2024-11-14 09:49:15 +0200

    Add checkboxes step definition class

---
+++ HerokuAppSeleniumTesting/StepDefinitions/Checkboxes/CheckboxesStepDefinitions.cs
@@ -0,0 +1,16 @@
+﻿using HerokuAppSeleniumTesting.Pages.Checkboxes;
+using TechTalk.SpecFlow;
+
+namespace HerokuAppSeleniumTesting.StepDefinitions.Checkboxes
+{
+	[Binding]
+	public class CheckboxesStepDefinitions
+	{
+		private readonly CheckboxesPage checkboxesPage;
+
+		public CheckboxesStepDefinitions(CheckboxesPage checkboxesPage)
+		{
+			this.checkboxesPage = checkboxesPage;
+		}
+	}
+}
