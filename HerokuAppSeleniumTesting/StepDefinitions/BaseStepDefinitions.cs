f596fd4908713331ad6d658f497568bb367fb280
Petya Yakimova <petya_yakimova@abv.bg>
2024-11-15 09:44:05 +0200

    Add base step definitions class

---
+++ HerokuAppSeleniumTesting/StepDefinitions/BaseStepDefinitions.cs
@@ -0,0 +1,15 @@
+﻿using TechTalk.SpecFlow;
+
+namespace HerokuAppSeleniumTesting.StepDefinitions
+{
+	[Binding]
+	public class BaseStepDefinitions
+	{
+		public BaseStepDefinitions() { }
+
+		protected bool GetBooleanFromString(string value)
+		{
+			return value.ToLower().Contains("not") ? false : true;
+		}
+	}
+}
