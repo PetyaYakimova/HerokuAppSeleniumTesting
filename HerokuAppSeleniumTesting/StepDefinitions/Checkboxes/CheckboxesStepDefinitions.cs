cc2cb3a4041dc9389acaed12ce5c6a01b274e2c6
Petya Yakimova <petya_yakimova@abv.bg>
2024-11-15 09:44:53 +0200

    Add steo definitions for checking if a checkbox is checked

--- HerokuAppSeleniumTesting/StepDefinitions/Checkboxes/CheckboxesStepDefinitions.cs
+++ HerokuAppSeleniumTesting/StepDefinitions/Checkboxes/CheckboxesStepDefinitions.cs
@@ -3,14 +3,27 @@ using TechTalk.SpecFlow;
 
 namespace HerokuAppSeleniumTesting.StepDefinitions.Checkboxes
 {
-	[Binding]
-	public class CheckboxesStepDefinitions
+	public class CheckboxesStepDefinitions : BaseStepDefinitions
 	{
 		private readonly CheckboxesPage checkboxesPage;
 
-		public CheckboxesStepDefinitions(CheckboxesPage checkboxesPage)
+		public CheckboxesStepDefinitions(CheckboxesPage checkboxesPage) : base()
 		{
 			this.checkboxesPage = checkboxesPage;
+		}
+
+		[Then(@"assert that the first checkbox (.*) checked")]
+		public void AssertThatTheFirstCheckboxChecked(string expectedCheckedString)
+		{
+			bool expectedChecked = GetBooleanFromString(expectedCheckedString);
+			Assert.That(checkboxesPage.IsCheckboxChecked(checkboxesPage.FirstCheckbox), Is.EqualTo(expectedChecked));
+		}
+
+		[Then(@"assert that the second checkbox (.*) checked")]
+		public void AssertThatTheSecondCheckboxChecked(string expectedCheckedString)
+		{
+			bool expectedChecked = GetBooleanFromString(expectedCheckedString);
+			Assert.That(checkboxesPage.IsCheckboxChecked(checkboxesPage.SecondCheckbox), Is.EqualTo(expectedChecked));
 		}
 	}
 }
