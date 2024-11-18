3f6206904ecac501566f4cce93728d53464e2cbd
Petya Yakimova <petya_yakimova@abv.bg>
2024-11-18 08:47:33 +0200

    Add context menu step definitions

---
+++ HerokuAppSeleniumTesting/StepDefinitions/ContextMenu/ContextMenuStepDefinitions.cs
@@ -0,0 +1,14 @@
+﻿using HerokuAppSeleniumTesting.Pages.ContextMenu;
+
+namespace HerokuAppSeleniumTesting.StepDefinitions.ContextMenu
+{
+	public class ContextMenuStepDefinitions : BaseStepDefinitions
+	{
+		private readonly ContextMenuPage contextMenuPage;
+
+		public ContextMenuStepDefinitions(ContextMenuPage contextMenuPage) : base()
+		{
+			this.contextMenuPage = contextMenuPage;
+		}
+	}
+}
