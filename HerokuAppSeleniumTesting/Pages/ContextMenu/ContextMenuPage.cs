ba809650511a683a5011d29998ee18c014a6314b
Petya Yakimova <petya_yakimova@abv.bg>
2024-11-18 08:45:47 +0200

    Add context menu page

---
+++ HerokuAppSeleniumTesting/Pages/ContextMenu/ContextMenuPage.cs
@@ -0,0 +1,14 @@
+﻿using HerokuAppSeleniumTesting.Setup;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace HerokuAppSeleniumTesting.Pages.ContextMenu
+{
+	public class ContextMenuPage : BasePage
+	{
+		public ContextMenuPage(AppSettings settings, IWebDriver driver, WebDriverWait wait)
+			: base(settings, driver, wait)
+		{
+		}
+	}
+}
