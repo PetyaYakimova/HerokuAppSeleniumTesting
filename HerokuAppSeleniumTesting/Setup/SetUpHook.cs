9c62c09d7e13419f3ac43b0a775bd9832b42b356
Petya Yakimova <petya_yakimova@abv.bg>
2024-11-19 09:39:05 +0200

    Update before scenario to be able to switch between browsers

--- HerokuAppSeleniumTesting/Setup/SetUpHook.cs
+++ HerokuAppSeleniumTesting/Setup/SetUpHook.cs
@@ -4,6 +4,7 @@ using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
 using OpenQA.Selenium;
 using TechTalk.SpecFlow;
+using OpenQA.Selenium.Firefox;
 
 namespace HerokuAppSeleniumTesting.Setup;
 
@@ -24,14 +25,11 @@ public class SetUpHook
 	[BeforeScenario]
 	public void Setup(IObjectContainer objectContainer, ScenarioContext scenarioContext)
 	{
-		ChromeOptions options = new ChromeOptions();
+		//IWebDriver driver = this.GetDriver("chrome");
+		IWebDriver driver = this.GetDriver("firefox");
 
-		//options.AddArgument("--headless");
-		options.AddArgument("--enable-automation");
-		options.AddArgument("--start-maximized");
-		options.PageLoadStrategy = PageLoadStrategy.Normal;
-		IWebDriver driver = new ChromeDriver(".", options);
 		driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(10);
+
 		this.wait = new WebDriverWait(driver, new TimeSpan(0, 0, 10));
 		objectContainer.RegisterInstanceAs(driver);
 		objectContainer.RegisterInstanceAs(wait);
@@ -53,5 +51,45 @@ public class SetUpHook
 #endif
 
 		return builder.Build();
+	}
+
+	private IWebDriver GetDriver(string browserName)
+	{
+		switch (browserName.ToLower())
+		{
+			case "chrome":
+				ChromeOptions options = new ChromeOptions();
+				//options.AddArgument("--headless");
+				options.AddArgument("--enable-automation");
+				options.AddArgument("--start-maximized");
+				options.PageLoadStrategy = PageLoadStrategy.Normal;
+				IWebDriver driver = new ChromeDriver(".", options);
+
+				return driver;
+
+			case "firefox":
+				FirefoxOptions firefoxOptions = new FirefoxOptions();
+				//firefoxOptions.AddArgument("--headless");
+				firefoxOptions.AddArgument("--enable-automation");
+				firefoxOptions.AddArgument("--start-maximized");
+				firefoxOptions.PageLoadStrategy = PageLoadStrategy.Normal;
+				IWebDriver firefoxDriver = new FirefoxDriver(".", firefoxOptions);
+
+				return firefoxDriver;
+
+			default:
+				throw new ArgumentException($"Browser {browserName} is not supported.");
+		}
+	}
+
+	private string GetBrowserFromTags(ScenarioContext scenarioContext)
+	{
+		// Look for browser tags in the scenario's tags
+		var browserTag = scenarioContext.ScenarioInfo.Tags
+							.FirstOrDefault(tag => tag.Equals("chrome", StringComparison.OrdinalIgnoreCase) ||
+												   tag.Equals("firefox", StringComparison.OrdinalIgnoreCase));
+
+		// Default to Chrome if no browser tag is found
+		return browserTag ?? "chrome";
 	}
 }
