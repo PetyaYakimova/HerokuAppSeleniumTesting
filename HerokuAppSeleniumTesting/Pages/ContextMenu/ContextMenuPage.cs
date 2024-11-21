3dd34fdeddb116dc2956fa9945ab77d668a215d4
Petya Yakimova <petya_yakimova@abv.bg>
2024-11-21 10:08:21 +0200

    Add locator and method for hot spot box

--- HerokuAppSeleniumTesting/Pages/ContextMenu/ContextMenuPage.cs
+++ HerokuAppSeleniumTesting/Pages/ContextMenu/ContextMenuPage.cs
@@ -1,5 +1,6 @@
 ﻿using HerokuAppSeleniumTesting.Setup;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
 using OpenQA.Selenium.Support.UI;
 
 namespace HerokuAppSeleniumTesting.Pages.ContextMenu;
@@ -9,5 +10,14 @@ public class ContextMenuPage : BasePage
 	public ContextMenuPage(AppSettings settings, IWebDriver driver, WebDriverWait wait)
 		: base(settings, driver, wait)
 	{
+	}
+
+	public IWebElement HotSpotBox => driver.FindElement(HotSpotBoxBy);
+	public By HotSpotBoxBy = By.Id("hot-spot");
+
+	public void RightClickHotSpotBox()
+	{
+		Actions actions = new Actions(driver);
+		actions.ContextClick(HotSpotBox).Perform();
 	}
 }
