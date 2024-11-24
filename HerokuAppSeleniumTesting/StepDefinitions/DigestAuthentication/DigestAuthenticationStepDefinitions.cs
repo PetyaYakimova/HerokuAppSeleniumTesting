b0d2e357988542d169e1f5d16c2eec67819a0e1e
Petya Yakimova <petya_yakimova@abv.bg>
2024-11-24 11:10:53 +0200

    Add digets authentication step definitions

---
+++ HerokuAppSeleniumTesting/StepDefinitions/DigestAuthentication/DigestAuthenticationStepDefinitions.cs
@@ -0,0 +1,14 @@
+﻿using HerokuAppSeleniumTesting.Pages.DigestAuthentications;
+
+namespace HerokuAppSeleniumTesting.StepDefinitions.DigestAuthentication
+{
+	public class DigestAuthenticationStepDefinitions : BaseStepDefinitions
+	{
+		private readonly DigestAuthenticationPage digestAuthenticationPage;
+
+		public DigestAuthenticationStepDefinitions(DigestAuthenticationPage digestAuthenticationPage) : base()
+		{
+			this.digestAuthenticationPage = digestAuthenticationPage;
+		}
+	}
+}
