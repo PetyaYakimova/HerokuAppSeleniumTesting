b46a6e0616c6024e420842858445f3bba6010db6
Petya Yakimova <petya_yakimova@abv.bg>
2024-11-24 11:10:44 +0200

    Add digest authentication page

---
+++ HerokuAppSeleniumTesting/Pages/DigestAuthentications/DigestAuthenticationPage.cs
@@ -0,0 +1,14 @@
+﻿using HerokuAppSeleniumTesting.Setup;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium;
+
+namespace HerokuAppSeleniumTesting.Pages.DigestAuthentications
+{
+	public class DigestAuthenticationPage : BasePage
+	{
+		public DigestAuthenticationPage(AppSettings settings, IWebDriver driver, WebDriverWait wait)
+			: base(settings, driver, wait)
+		{
+		}
+	}
+}
