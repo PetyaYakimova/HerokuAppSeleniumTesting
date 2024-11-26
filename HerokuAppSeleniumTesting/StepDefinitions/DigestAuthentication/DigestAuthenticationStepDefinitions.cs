61c836ac4f781ad0b03c61123b323a94ca964b17
Petya Yakimova <petya_yakimova@abv.bg>
2024-11-26 19:58:03 +0200

    Add step definition to check you are actually logged in

--- HerokuAppSeleniumTesting/StepDefinitions/DigestAuthentication/DigestAuthenticationStepDefinitions.cs
+++ HerokuAppSeleniumTesting/StepDefinitions/DigestAuthentication/DigestAuthenticationStepDefinitions.cs
@@ -1,21 +1,26 @@
 ﻿using HerokuAppSeleniumTesting.Pages.DigestAuthentications;
 using TechTalk.SpecFlow;
 
-namespace HerokuAppSeleniumTesting.StepDefinitions.DigestAuthentication
+namespace HerokuAppSeleniumTesting.StepDefinitions.DigestAuthentication;
+
+public class DigestAuthenticationStepDefinitions : BaseStepDefinitions
 {
-	public class DigestAuthenticationStepDefinitions : BaseStepDefinitions
+	private readonly DigestAuthenticationPage digestAuthenticationPage;
+
+	public DigestAuthenticationStepDefinitions(DigestAuthenticationPage digestAuthenticationPage) : base()
 	{
-		private readonly DigestAuthenticationPage digestAuthenticationPage;
+		this.digestAuthenticationPage = digestAuthenticationPage;
+	}
 
-		public DigestAuthenticationStepDefinitions(DigestAuthenticationPage digestAuthenticationPage) : base()
-		{
-			this.digestAuthenticationPage = digestAuthenticationPage;
-		}
+	[StepDefinition(@"I pass username (.*) and password (.*) for the digest authentication")]
+	public void PassUsernameAndPasswordForTheDigestAuthentication(string username, string password)
+	{
+		digestAuthenticationPage.PassUsernameAndPasswordForDigestAuth(username, password);
+	}
 
-		[StepDefinition(@"I pass username (.*) and password (.*) for the digest authentication")]
-		public void PassUsernameAndPasswordForTheDigestAuthentication(string username, string password)
-		{
-			this.digestAuthenticationPage.PassUsernameAndPasswordForDigestAuth(username, password);
-		}
+	[Then(@"assert that I see the digest authentication page heading")]
+	public void AssertThatISeeTheDigestAuthenticationPageHeading()
+	{
+		Assert.IsTrue(digestAuthenticationPage.Heading.Displayed);
 	}
 }
