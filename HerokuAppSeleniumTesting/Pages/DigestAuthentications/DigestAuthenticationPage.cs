b4e98eb66a743b1e51951e6eb1835756c8be55c8
Petya Yakimova <petya_yakimova@abv.bg>
2024-11-26 19:58:12 +0200

    Add lcoator for heading element

--- HerokuAppSeleniumTesting/Pages/DigestAuthentications/DigestAuthenticationPage.cs
+++ HerokuAppSeleniumTesting/Pages/DigestAuthentications/DigestAuthenticationPage.cs
@@ -2,19 +2,21 @@
 using OpenQA.Selenium.Support.UI;
 using OpenQA.Selenium;
 
-namespace HerokuAppSeleniumTesting.Pages.DigestAuthentications
-{
-	public class DigestAuthenticationPage : BasePage
-	{
-		public DigestAuthenticationPage(AppSettings settings, IWebDriver driver, WebDriverWait wait)
-			: base(settings, driver, wait)
-		{
-		}
+namespace HerokuAppSeleniumTesting.Pages.DigestAuthentications;
 
-		public void PassUsernameAndPasswordForDigestAuth(string username, string pass)
-		{
-			string url = "https://" + username + ":" + pass + "@" + settings.DomainSettings.Domain.Replace("https://", string.Empty) + "/digest_auth";
-			driver.Navigate().GoToUrl(url);
-		}
+public class DigestAuthenticationPage : BasePage
+{
+	public DigestAuthenticationPage(AppSettings settings, IWebDriver driver, WebDriverWait wait)
+		: base(settings, driver, wait)
+	{
+	}
+
+	public IWebElement Heading => driver.FindElement(HeadingBy);
+	public By HeadingBy = By.TagName("h3");
+
+	public void PassUsernameAndPasswordForDigestAuth(string username, string pass)
+	{
+		string url = "https://" + username + ":" + pass + "@" + settings.DomainSettings.Domain.Replace("https://", string.Empty) + "digest_auth";
+		driver.Navigate().GoToUrl(url);
 	}
 }
