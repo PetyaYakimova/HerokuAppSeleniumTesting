b09108da6e315a33803536a2b25a22f0a4df79d2
Petya Yakimova <petya_yakimova@abv.bg>
2024-11-28 09:35:41 +0200

    add drag and drop step definitions

---
+++ HerokuAppSeleniumTesting/StepDefinitions/DragAndDrop/DragAndDropStepDefinitions.cs
@@ -0,0 +1,14 @@
+﻿using HerokuAppSeleniumTesting.Pages.DragAndDrop;
+
+namespace HerokuAppSeleniumTesting.StepDefinitions.DragAndDrop
+{
+	public class DragAndDropStepDefinitions : BaseStepDefinitions
+	{
+		private readonly DragAndDropPage dragAndDropPage;
+
+		public DragAndDropStepDefinitions(DragAndDropPage dragAndDropPage) : base()
+		{
+			this.dragAndDropPage = dragAndDropPage;
+		}
+	}
+}
