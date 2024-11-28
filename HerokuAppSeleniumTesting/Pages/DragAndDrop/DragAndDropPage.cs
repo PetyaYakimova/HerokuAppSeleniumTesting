bd799e43f9221435c5b9fb3e06dfd7fa7a222d60
Petya Yakimova <petya_yakimova@abv.bg>
2024-11-28 09:35:33 +0200

    add drag and drop page

---
+++ HerokuAppSeleniumTesting/Pages/DragAndDrop/DragAndDropPage.cs
@@ -0,0 +1,20 @@
+﻿using HerokuAppSeleniumTesting.Setup;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium;
+
+namespace HerokuAppSeleniumTesting.Pages.DragAndDrop
+{
+	public class DragAndDropPage : BasePage
+	{
+		public DragAndDropPage(AppSettings settings, IWebDriver driver, WebDriverWait wait)
+		: base(settings, driver, wait)
+		{
+		}
+
+		public IWebElement ColumnA => driver.FindElement(ColumnABy);
+		public By ColumnABy = By.Id("column-a");
+
+		public IWebElement ColumnB => driver.FindElement(ColumnBBy);
+		public By ColumnBBy = By.Id("column-b");
+	}
+}
