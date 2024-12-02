af36cbd8d996aa97aa8bd0e1b7154cb18793c843
Petya Yakimova <petya_yakimova@abv.bg>
2024-12-02 09:14:35 +0200

    Add sortable data tables page

---
+++ HerokuAppSeleniumTesting/Pages/SortableDataTables/SortableDataTablesPage.cs
@@ -0,0 +1,13 @@
+﻿using HerokuAppSeleniumTesting.Setup;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium;
+
+namespace HerokuAppSeleniumTesting.Pages.SortableDataTables;
+
+public class SortableDataTablesPage : BasePage
+{
+	public SortableDataTablesPage(AppSettings settings, IWebDriver driver, WebDriverWait wait)
+	: base(settings, driver, wait)
+	{
+	}
+}
