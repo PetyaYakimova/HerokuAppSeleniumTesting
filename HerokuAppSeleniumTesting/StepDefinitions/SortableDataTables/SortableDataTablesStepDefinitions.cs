c35df91e51840ab1e248913d2d5bfb5c4ee9953d
Petya Yakimova <petya_yakimova@abv.bg>
2024-12-02 09:14:50 +0200

    Add sortable data tables step definitions

---
+++ HerokuAppSeleniumTesting/StepDefinitions/SortableDataTables/SortableDataTablesStepDefinitions.cs
@@ -0,0 +1,13 @@
+﻿using HerokuAppSeleniumTesting.Pages.SortableDataTables;
+
+namespace HerokuAppSeleniumTesting.StepDefinitions.SortableDataTables;
+
+public class SortableDataTablesStepDefinitions : BaseStepDefinitions
+{
+	private readonly SortableDataTablesPage sortableDataTablesPage;
+
+	public SortableDataTablesStepDefinitions(SortableDataTablesPage sortableDataTablesPage) : base()
+	{
+		this.sortableDataTablesPage = sortableDataTablesPage;
+	}
+}
