96d53abee8fa827ac4fa9d4055c56f7ca2f9997a
Petya Yakimova <petya_yakimova@abv.bg>
2024-12-04 10:10:16 +0200

    Add method to get all values fro the first column

--- HerokuAppSeleniumTesting/Pages/SortableDataTables/SortableDataTablesPage.cs
+++ HerokuAppSeleniumTesting/Pages/SortableDataTables/SortableDataTablesPage.cs
@@ -11,8 +11,16 @@ public class SortableDataTablesPage : BasePage
 	{
 	}
 
+	public IWebElement FirstTable_ValueInLastNameColumn => driver.FindElement(FirstTable_ValueInLastNameColumnBy);
+	public By FirstTable_ValueInLastNameColumnBy = By.XPath("//table[@id='table1']//tbody//tr//td[1]");
+
 	public void ClickTableColumnHeaderInFirstTable(string columnName)
 	{
 		driver.FindElement(By.XPath($"//table[@id='table1']//th//span[text()='{columnName}']")).Click();
+	}
+
+	public List<string> GetAllValuesFromTableInLastNameColumn()
+	{
+		return driver.FindElements(FirstTable_ValueInLastNameColumnBy).Select(e => e.Text).ToList();
 	}
 }
