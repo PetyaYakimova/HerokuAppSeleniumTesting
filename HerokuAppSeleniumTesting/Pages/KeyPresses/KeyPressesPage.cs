6a2af3168c3fb865b92b24cef72961192f702237
Petya Yakimova <petya_yakimova@abv.bg>
2024-12-15 11:12:47 +0200

    Add key presses page

---
+++ HerokuAppSeleniumTesting/Pages/KeyPresses/KeyPressesPage.cs
@@ -0,0 +1,13 @@
+﻿using HerokuAppSeleniumTesting.Setup;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium;
+
+namespace HerokuAppSeleniumTesting.Pages.KeyPresses;
+
+public class KeyPressesPage : BasePage
+{
+	public KeyPressesPage(AppSettings settings, IWebDriver driver, WebDriverWait wait)
+		: base(settings, driver, wait)
+	{
+	}
+}
