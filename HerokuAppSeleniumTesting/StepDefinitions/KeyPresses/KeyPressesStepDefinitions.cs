999cd4998dcf9206d9f53ea2780eef4f379989e0
Petya Yakimova <petya_yakimova@abv.bg>
2024-12-15 11:13:00 +0200

    Add key presses step deifnitions

---
+++ HerokuAppSeleniumTesting/StepDefinitions/KeyPresses/KeyPressesStepDefinitions.cs
@@ -0,0 +1,13 @@
+﻿using HerokuAppSeleniumTesting.Pages.KeyPresses;
+
+namespace HerokuAppSeleniumTesting.StepDefinitions.KeyPresses;
+
+public class KeyPressesStepDefinitions : BaseStepDefinitions
+{
+	private readonly KeyPressesPage keyPressesPage;
+
+	public KeyPressesStepDefinitions(KeyPressesPage keyPressesPage) : base()
+	{
+		this.keyPressesPage = keyPressesPage;
+	}
+}
