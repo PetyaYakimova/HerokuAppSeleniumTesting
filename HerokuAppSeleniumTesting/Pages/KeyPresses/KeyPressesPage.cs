dca4de80dfde6d576f8d87fc0031d61730169c91
Petya Yakimova <petya_yakimova@abv.bg>
2024-12-16 09:22:17 +0200

    Add method for sending key to the page

--- HerokuAppSeleniumTesting/Pages/KeyPresses/KeyPressesPage.cs
+++ HerokuAppSeleniumTesting/Pages/KeyPresses/KeyPressesPage.cs
@@ -1,6 +1,7 @@
 ﻿using HerokuAppSeleniumTesting.Setup;
 using OpenQA.Selenium.Support.UI;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
 
 namespace HerokuAppSeleniumTesting.Pages.KeyPresses;
 
@@ -9,5 +10,11 @@ public class KeyPressesPage : BasePage
 	public KeyPressesPage(AppSettings settings, IWebDriver driver, WebDriverWait wait)
 		: base(settings, driver, wait)
 	{
+	}
+
+	public void SendKeyToThePage(string key)
+	{
+		Actions actions = new Actions(driver);
+		actions.SendKeys(key).Perform();
 	}
 }
