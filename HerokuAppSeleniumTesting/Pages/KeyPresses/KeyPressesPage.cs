4638c3a3a3365ee10c793a8b35d1da7047c04905
Petya Yakimova <petya_yakimova@abv.bg>
2024-12-17 09:35:30 +0200

    Add method for getting the result text

--- HerokuAppSeleniumTesting/Pages/KeyPresses/KeyPressesPage.cs
+++ HerokuAppSeleniumTesting/Pages/KeyPresses/KeyPressesPage.cs
@@ -7,14 +7,26 @@ namespace HerokuAppSeleniumTesting.Pages.KeyPresses;
 
 public class KeyPressesPage : BasePage
 {
+	private const string ResultStartText = "You entered: ";
+
 	public KeyPressesPage(AppSettings settings, IWebDriver driver, WebDriverWait wait)
 		: base(settings, driver, wait)
 	{
 	}
 
+	public IWebElement ResultText => driver.FindElement(ResultTextBy);
+	public By ResultTextBy = By.Id("result");
+
 	public void SendKeyToThePage(string key)
 	{
 		Actions actions = new Actions(driver);
 		actions.SendKeys(key).Perform();
+	}
+
+	public string GetValueFromResultText()
+	{
+		string resultText = ResultText.Text;
+
+		return resultText.Replace(ResultStartText, string.Empty);
 	}
 }
