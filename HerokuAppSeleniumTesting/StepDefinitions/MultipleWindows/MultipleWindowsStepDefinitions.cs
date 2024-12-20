6dad6ded0c84e69a14aca6f5e25cf185395770c3
Petya Yakimova <petya_yakimova@abv.bg>
2024-12-20 09:37:25 +0200

    Add multiple windows step definitions

---
+++ HerokuAppSeleniumTesting/StepDefinitions/MultipleWindows/MultipleWindowsStepDefinitions.cs
@@ -0,0 +1,13 @@
+﻿using HerokuAppSeleniumTesting.Pages.MultipleWindows;
+
+namespace HerokuAppSeleniumTesting.StepDefinitions.MultipleWindows;
+
+public class MultipleWindowsStepDefinitions : BaseStepDefinitions
+{
+	private readonly MultipleWindowsPage multipleWindowsPage;
+
+	public MultipleWindowsStepDefinitions(MultipleWindowsPage multipleWindowsPage) : base()
+	{
+		this.multipleWindowsPage = multipleWindowsPage;
+	}
+}
