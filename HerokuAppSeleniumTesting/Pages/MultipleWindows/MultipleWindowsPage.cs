6d07110f2cbed4f4b5aafe764944a44a69076e10
Petya Yakimova <petya_yakimova@abv.bg>
2024-12-20 09:37:13 +0200

    Add multiple windows page

---
+++ HerokuAppSeleniumTesting/Pages/MultipleWindows/MultipleWindowsPage.cs
@@ -0,0 +1,13 @@
+﻿using HerokuAppSeleniumTesting.Setup;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium;
+
+namespace HerokuAppSeleniumTesting.Pages.MultipleWindows;
+
+public class MultipleWindowsPage : BasePage
+{
+	public MultipleWindowsPage(AppSettings settings, IWebDriver driver, WebDriverWait wait)
+	: base(settings, driver, wait)
+	{
+	}
+}
